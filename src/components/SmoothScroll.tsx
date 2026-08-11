89235b760b6febbc7b60a141ee6ca649df62f80a
santiagosuarez32 <dlsantii86@gmail.com>
2026-08-10 23:19:15 -0300

    ssdfsdf

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -3,6 +3,10 @@
 import { ReactNode, useEffect, useRef } from "react";
 import Lenis from "lenis";
 import { usePathname } from "next/navigation";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 declare global {
   interface Window {
@@ -27,6 +31,9 @@ export default function SmoothScroll({ children }: { children: ReactNode }) {
 
     lenisRef.current = lenis;
     window.__lenis = lenis;
+
+    // Sync Lenis scroll with GSAP ScrollTrigger
+    lenis.on("scroll", ScrollTrigger.update);
 
     function raf(time: number) {
       lenis.raf(time);
